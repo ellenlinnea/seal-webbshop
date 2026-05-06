0c037b7c3f7fcdab8429a95ea9f5b70886db4e16
ellenlinnea <linnea.schildt1@gmail.com>
2026-05-06 12:00:45 +0200

    set-up av projektet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Hem</div>} />
+        <Route path="/salar" element={<div>Sälbutiken</div>} />
+        <Route path="/salar/:id" element={<div>Säl-detalj</div>} />
+        <Route path="/varukorg" element={<div>Varukorg</div>} />
+        <Route path="/kassa" element={<div>Kassa</div>} />
+        <Route path="/bekraftelse" element={<div>Bekräftelse</div>} />
+        <Route path="/favoriter" element={<div>Favoriter</div>} />
+        <Route path="/login" element={<div>Logga in</div>} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
